0676c7b4265b14dba394e07c1f19802fb13769ef
Brookann <blaire08@ymail.com>
2021-02-05 17:20:21 -0600

    Initial Data

---
+++ PyBank/main.py
@@ -0,0 +1,30 @@
+#My to do list
+# Create the total number of months included in the dataset
+# Create the net total amount of "Profit/Losses" over the entire period
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# Create the greatest increase in profits (date and amount) over the entire period
+# Create the greatest decrease in losses (date and amount) over the entire period
+# Make final script should both print the analysis to the terminal and export a text file with the results.
+----------------------------------------------------------------------------------------------------------------------
+
+# Import both the os and csv
+import os
+import csv
+
+# Make the place for the csv file for PyBank
+PyBankcsv = os.path.join("Resources","budget_data.csv")
+
+# Make a place for the the data
+profit = []
+monthly_changes = []
+date = []
+
+# Input variables
+initial_profit = 0
+total_profit = 0
+total_change_profits = 0
+count = 0
+
+#Open csv
+with open(PyBankcsv) as Bank_file :
+
---
+++ PyPoll/main.py
@@ -0,0 +1,24 @@
+# My to do list.
+# Create the total number of votes cast
+# Create a complete list of candidates who received votes
+# Create the percentage of votes each candidate won
+# Create the total number of votes each candidate won
+# Create the winner of the election based on popular vote
+-----------------------------------------------------------------------------
+
+# Import both the os and csv
+import os
+import csv
+
+# Make the place for the csv file for PyPoll
+PyPollcsv = os.path.join("Resources","election_data.csv")
+
+# Make a place for the data
+vote_count = []
+vote_percent = []
+candidate_list = []
+unique_candidate = []
+count = 0
+
+# Open csv
+with open(PyPollcsv) as Bank_file :
