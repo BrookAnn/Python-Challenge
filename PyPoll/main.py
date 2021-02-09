f0d2ee8e54a51c828b8d4d26db41eb162291a395
Brookann <blaire08@ymail.com>
2021-02-08 20:43:33 -0600

    Added i-k loop

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,12 +17,20 @@ import csv
 PyPollcsv = os.path.join("Resources/election_data.csv")
 out_file="./Analysis/output.txt"
 
-# Make a place for the data
+#---------------------------------------------------------------------------------------------------------
+
+# Make a place for the the data
+voter_id = []
+county = []
+candidate = []
+#---------------------------------------------------------------------------------------------------------
+
+# Make variables
 vote_count = []
-vote_percent = []
-candidate_list = []
-winning_candidate = []
+candidate_list = {"Candidate": [], "Votes": [], "Vote Perc": []}
+unique_candidate = []
 count = 0
+win = 0
 
 #----------------------------------------------------------------------------------------------------------
 
@@ -31,20 +39,45 @@ with open(PyPollcsv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
 
+#----------------------------------------------------------------------------------------------------------
+
+#Start loop
     for i in csvreader:
         count = count + 1
-        candidate_list.append(i[2])
-    for j in set(candidate_list):
-        winning_candidate.append(j)
-        k = candidate_list.count(j)
-        vote_count.append(k)
-        l = (k/count)*100
-        vote_percent.append(l)
+        unique_candidate.append(i[2])
+
+        if i[2] not in candidate_list["Candidate"]:
+            candidate_list["Candidate"].append(i)[2]
+
+    for j in candidate_list["Candidate"]:
+        candidate_list["Votes"].append(unique_candidate.count(j))
+    for k in candidate_list["Votes"]:
+        percent = (k/count)*100
+        candidate_list["Vote Perc"].append(round(percent, 3))
+
+    TopDog = max(candidate_list["Votes"])
+    VoterDex = candidate_list["Votes"].index(TopDog)
+    Winning = candidate_list["Candidate"][VoterDex]
+        #unique_candidate.append(j)
+        #k = candidate_list.count(j)
+        #vote_count.append(k)
+        #l = (k/count)*100
+
+ #-----------------------------------------------------------------------------------------------------------
 
 #Print Profit
-print(f"vote_count:")
 print(f"Financial Analysis")
-print(f"vote_percent:")
-print(f"candadite_list")
-print(f"winning_candidate")
+print(f"vote_count:{(count)}")
+print("The winner is:")
+print(f"unique_candidate:{(candidate_list)}")
+#print(f"winning_candidate")
 
+#------------------------------------------------------------------------------------------------------------
+
+#Make Analysis
+#with open('financial_analysis.txt', 'w') as text:
+    #text.write(f"vote_count:{count}")
+    #text.write(f"FinancialAnalysis")
+    #text.write(f"vote_percent:")
+    #text.write(f"candadite_list:")
+    #text.write(f"winning_candidate")
